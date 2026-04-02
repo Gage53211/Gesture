b7246b4ec3e2492cf44b0933026bf2297d46bed4
Gage53211 <gagemather@gmail.com>
2026-04-02 11:51:31 -0600

    added basic code to display metadata

--- app/src/main/java/com/example/button_application/MainActivity.kt
+++ app/src/main/java/com/example/button_application/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.button_application
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
@@ -13,15 +16,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import coil3.compose.AsyncImage
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImagePainter
 import com.example.button_application.ui.theme.Button_applicationTheme
+import androidx.core.net.toUri
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
+    var picture by mutableStateOf<String?>("None")
+    var title by mutableStateOf<String?>("None")
+    var author by mutableStateOf<String?>("None")
+    var album by mutableStateOf<String?>("None")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val metaFilter = IntentFilter("ACTION_METADATA")
+        registerReceiver(metaReceiver, metaFilter, RECEIVER_EXPORTED)
 
         // Prompt user for permission if not already granted
         if (!isNotificationServiceEnabled()) {
@@ -40,11 +59,22 @@ class MainActivity : ComponentActivity() {
                         onPlayClick = { sendPlayBroadcast() },
                         onNextAppClick = { sendNextAppBroadcast() },
                         onPrevAppClick = { sendPrevAppBroadcast() },
+                        onLikeClick = { sendLikeBroadcast() },
+                        onDislikeClick = { sendDislikeBroadcast() },
+                        URI = picture,
+                        title = title,
+                        author = author,
+                        album = album,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(metaReceiver)
+        super.onDestroy()
     }
 
     private fun sendSkipBroadcast() {
@@ -87,10 +117,35 @@ class MainActivity : ComponentActivity() {
         sendBroadcast(intent)
     }
 
+    private fun sendLikeBroadcast() {
+        val intent = Intent("ACTION_LIKE")
+        sendBroadcast(intent)
+    }
+
+    private fun sendDislikeBroadcast() {
+        val intent = Intent("ACTION_DISLIKE")
+        sendBroadcast(intent)
+    }
+
     private fun isNotificationServiceEnabled(): Boolean {
         val pkgName = packageName
         val flat = Settings.Secure.getString(contentResolver, "enabled_notification_listeners")
         return flat?.contains(pkgName) == true
+    }
+
+    //metadata receiver
+    private val metaReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            println("--------------------------------------------------")
+            println("Sessions Tracked: ${intent?.getIntExtra("SESSIONS_TRACKED", 0)}")
+            println("--------------------------------------------------")
+
+            picture = intent?.getStringExtra("URI") ?: "None"
+            title = intent?.getStringExtra("TITLE")
+            album = intent?.getStringExtra("ALBUM_NAME")
+            author = intent?.getStringExtra("AUTHOR")
+
+        }
     }
 }
 
@@ -103,6 +158,12 @@ fun MediaControlScreen(onSkipClick: () -> Unit,
                        onPlayClick: () -> Unit,
                        onNextAppClick: () -> Unit,
                        onPrevAppClick: () -> Unit,
+                       onLikeClick: () -> Unit,
+                       onDislikeClick: () -> Unit,
+                       URI: String?,
+                       title: String?,
+                       album: String?,
+                       author: String?,
                        modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.fillMaxSize(),
@@ -132,6 +193,37 @@ fun MediaControlScreen(onSkipClick: () -> Unit,
         }
         Button(onClick = onPrevAppClick) {
             Text(text = "Prev App")
+        }
+        Button(onClick = onLikeClick) {
+            Text(text = "Like")
+        }
+        Button(onClick = onDislikeClick) {
+            Text(text = "Dislike")
+        }
+        AsyncImage(
+            model = (URI ?: "").toUri(),
+            contentDescription = "Album Art",
+            modifier = Modifier.size(200.dp),
+            onState = { state ->
+                if (state is AsyncImagePainter.State.Error) {
+                    println("Error: ${state.result.throwable}")
+                }
+            }
+        )
+        if (title != null) {
+            Text(
+                text=title
+            )
+        }
+        if (author != null) {
+            Text(
+                text=author
+            )
+        }
+        if (album != null) {
+            Text(
+                text=album
+            )
         }
     }
 }
