5d59ced70c43ab2242d51834a008430a3d511fdd
Gage53211 <gagemather@gmail.com>
2026-04-26 17:04:07 -0600

    removed button logic

--- app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/TutorialActivity.java
+++ app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/TutorialActivity.java
@@ -1,8 +1,6 @@
 package com.google.mediapipe.examples.gesturerecognizer;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TutorialActivity extends AppCompatActivity {
@@ -11,15 +9,5 @@ public class TutorialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
-
-        Button myButton = (Button) findViewById(R.id.backButton);
-
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Closes tutorial and reveals the already-running MainActivity
-                finish();
-            }
-        });
     }
 }
